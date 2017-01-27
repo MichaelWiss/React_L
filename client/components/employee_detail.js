4790aaf4d0669e8a417b747c3e744036ffba2684
MichaelWiss <michael.wiss@gmail.com>
2017-01-27 15:49:28 -0600

    bootstrap added

--- client/components/employee_detail.js
+++ client/components/employee_detail.js
@@ -8,6 +8,10 @@ const EmployeeDetail = ({employee}) => {
           <img src={avatar} />
           <div className="caption">
           <h3>{name}</h3>
+          <ul className="list-group">
+	          <li className='list-group-item'>Email: {email}</li>
+	          <li className='list-group-item'>Phone: {phone}</li>
+          </ul>
           </div>
 		</div>
 		);
