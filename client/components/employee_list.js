5d371cc0f0e978f8fdcf44e5582ff762201bdd72
MichaelWiss <michael.wiss@gmail.com>
2017-01-30 15:16:14 -0600

    key

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -10,7 +10,7 @@ const EmployeeList = (props) => {
 	return (
         <div> 
           <div className="employee-list">
-             {props.employees.map(employee => <EmployeeDetail employee={employee} />)}
+             {props.employees.map(employee => <EmployeeDetail key={employee._id} employee={employee} />)}
           </div>
           </div>
         );
