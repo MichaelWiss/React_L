4fccfcaa9cd75bf370abecb9edd7e4897565540f
MichaelWiss <michael.wiss@gmail.com>
2017-01-31 13:20:43 -0600

    increment

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -7,6 +7,14 @@ const PER_PAGE = 20;
 
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.page = 1;
+  }
+
+  handleButtonClick(){
+    Meteor.subscribe('employees', PER_PAGE * this.page );
+    this.page +=1;
+  }
   render(){
   //props.employees => an array of objescts
 	console.log(props.employees);
@@ -15,7 +23,7 @@ class EmployeeList extends Component {
             <div className="employee-list">
                {this.props.employees.map(employee => <EmployeeDetail key={employee._id} employee={employee} />)}
             </div>
-            <button onClick={() => Meteor.subscribe('employees', 40)} 
+            <button onClick={this.handleButtonClick.bind(this)} 
             className="btn btn-primary">
             Load more...
             </button>
